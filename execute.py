c485343fa006bd4b9513d2485b7658f8c8f67ef3
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-22 19:24:39 +0100

    vms backup development

--- execute.py
+++ execute.py
@@ -4,6 +4,7 @@ import subprocess
 def ok(command: str, silent=False):
     print(f'executing: {command}')
     p = subprocess.Popen(command.split(' '),
+                         bufsize=0,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
     for line in iter(p.stdout.readline, b''):
