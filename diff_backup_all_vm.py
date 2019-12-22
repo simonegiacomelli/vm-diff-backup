2812a2f5c7139242ea95a07d44f5ed08a3658616
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-22 19:50:12 +0100

    vms backup development

--- diff_backup_all_vm.py
+++ diff_backup_all_vm.py
@@ -8,8 +8,16 @@ from execute import ok
 FAIL = 1
 SUCCESS = 0
 
-vm_list = ['200']
-
+vm_list = ['200', '300']
 folder = os.path.dirname(sys.argv[0])
+
+failed = []
 for vm in vm_list:
-    ok(f'{folder}/lvm_diff_backup.py {vm}')
+    if not ok(f'{folder}/lvm_diff_backup.py {vm}'):
+        failed.append(vm)
+
+if len(failed) > 0:
+    print(f'This vm backup failed: {failed}')
+    exit(FAIL)
+else:
+    exit(SUCCESS)
--- execute.py
+++ execute.py
@@ -1,17 +1,17 @@
 import subprocess
 
 
-def ok(command: str, silent=False):
-    print(f'executing: {command}')
+def ok(command: str, silent=False, log=print):
+    log(f'executing: {command}', flush=True)
     p = subprocess.Popen(command.split(' '),
                          bufsize=0,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
     for line in iter(p.stdout.readline, b''):
-        print('  ' + line.decode('utf-8').rstrip(), flush=True)
+        log('  ' + line.decode('utf-8').rstrip(), flush=True)
     p.wait(10)
     result = p.returncode
     success = result == 0
     if not success and not silent:
-        print(f'command failed result code: {result}')
+        log(f'command failed result code: {result}', flush=True)
     return success
