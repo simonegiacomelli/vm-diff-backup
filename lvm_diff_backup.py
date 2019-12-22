2003fbd374f8aa5c3255632393d17362fcbb789a
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-22 11:46:35 +0100

    first prod version

--- lvm_diff_backup.py
+++ lvm_diff_backup.py
@@ -1,16 +1,9 @@
-import os
+import datetime
 import subprocess
 import sys
-from pathlib import Path
-import datetime
-
-
-def log(line):
-    print(line)
-
 
 if len(sys.argv) <= 1:
-    log(f'Usage: {sys.argv[0]} VMID')
+    print(f'Usage: {sys.argv[0]} VMID')
     exit(1)
 
 vmid = sys.argv[1]
@@ -22,55 +15,38 @@ data_device = '/dev/pve/data'
 source_folder = f'/mnt/qm-backup/images/{vmid}'
 backup_folder = f'/mnt/lvdump/xdelta3/{year_month}/backup-{vmid}'
 
+lines = f"""
 
-def cmd(command, ignore_error=False):
-    def execute():
-        log(f'executing: {command}')
-        res = subprocess.call(command.split(' '))
-        if res != 0:
-            log(f'  ' + 'ignoring ' if ignore_error else '') + 'failed result code={res}'
-            if not ignore_error:
-                return 1
+#ignore_error=True
+umount {mount_point}
+lvremove -f {snapshot_device}
 
-    return execute
-
-
-def wrap(*args):
-    return [cmd(c) for c in args]
-
-
-def main():
-    init_fin = [cmd(f'umount {mount_point}', ignore_error=True),
-                cmd(f'lvremove -f {snapshot_device}', ignore_error=True)]
-
-    all_commands = init_fin + \
-                   wrap(f'lvcreate -L50G -s -n {snapshot_device} {data_device}',
-                        f'mkdir -p {mount_point}',
-                        f'mount {snapshot_device} {mount_point}',
-                        f'xbackup.py --backupfolder={backup_folder} --sourcefolder={source_folder}'
-                        ) + \
-                   init_fin
-
-    for command in all_commands:
-        if not command():
-            exit(1)
-    exit(0)
-
-
-if __name__ == '__main__':
-    main()
-commands = f"""
-umount {mount_point} #ignore_error 
-lvremove -f {snapshot_device} #ignore_error
-
+#ignore_error=False
 mkdir -p {mount_point}
 
 lvcreate -L50G -s -n {snapshot_device} {data_device}
 mount {snapshot_device} {mount_point}                                                                                                               
 xbackup.py --backupfolder={backup_folder} --sourcefolder={source_folder}                                                                                  
 
-umount {mount_point} #ignore_error
-lvremove -f {snapshot_device} #ignore_error                                                                                                   
+#ignore_error=True
+umount {mount_point}
+lvremove -f {snapshot_device}                                                                                                
 
-"""
+""".splitlines(keepends=False)
 
+ignore_error = False
+commands = [line for line in lines if line.strip() != '']
+
+for command in commands:
+    if command.startswith('#'):
+        exec(command[1:])
+        continue
+
+    print(f'executing: {command}')
+    res = subprocess.call(command.split(' '))
+    if res != 0:
+        print(f'  ' + ('ignoring ' if ignore_error else '') + 'failed result code={res}')
+        if not ignore_error:
+            exit(1)
+
+exit(0)
