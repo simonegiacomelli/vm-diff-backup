6ec1ce908bad1e8d1af450f75ac86f7c07b1668e
Simone Giacomelli <simone.giacomelli@gmail.com>
2019-12-22 22:33:56 +0100

    vms backup development

--- diff_backup_all_vm.py
+++ diff_backup_all_vm.py
@@ -112,6 +112,7 @@ def backup_all():
         if not backup(vm_id, source_folder, backup_folder, data_device, snapshot_device, mount_point):
             failed.append(vm_id)
         tee.indent_dec()
+        tee.log('')
         tee.close(log_file)
 
     success = len(failed) == 0
